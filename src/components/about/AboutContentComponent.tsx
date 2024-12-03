4e78b1d3ffc1030d557eb6732b7c58ffaaa60b98
KIMSOOJIN97 <kipper152@naver.com>
2024-12-03 10:36:52 +0900

    컴포넌트들 추가 - 각 페이지 ( about, project)에서 호출되는 컴포넌트 추가

---
+++ src/components/about/AboutContentComponent.tsx
@@ -0,0 +1,25 @@
+import "@/app/about/about.css";
+export default function AboutContentComponent() {
+  return (
+    <div className="text-container">
+      <p>안녕하세요! </p>
+      <p>
+        프론트엔드와 백엔드를 아우르며, 완성도 높은 웹 애플리케이션을 개발하는
+        4년차 풀스택 개발자 김수진입니다.
+      </p>
+      <p>
+        React와 TypeScript를 활용한 직관적이고 효율적인 UI 설계부터, Spring
+        Framework, Tibero를 기반으로 안정적이고 확장 가능한 서버 구축까지 경험이
+        풍부합니다.
+      </p>
+      <p>
+        다양한 프로젝트를 통해 사용자 중심의 문제를 해결하며, 기술적 혁신과
+        팀워크를 통해 더 나은 디지털 솔루션을 만들어가고 있습니다.
+      </p>
+      <p>
+        포트폴리오를 확인하시거나 함께 멋진 프로젝트를 만들어가고 싶으시다면
+        언제든 연락주세요!
+      </p>
+    </div>
+  );
+}
---
+++ src/components/common/HeaderComponent.tsx
@@ -0,0 +1,24 @@
+import "./Header.css";
+import Image from "next/image";
+
+export default function HeaderComponent() {
+  const headers = ["ABOUT", "PROJECTS", "SKILLS", "CONTACT"];
+  return (
+    <div className="header">
+      <Image
+        key={"soojinLogo"}
+        src={`/images/soojin_logo.png`}
+        alt={"soojinLogo"}
+        width={200}
+        height={5}
+      />
+      <ul className="header-container">
+        {headers.map((header) => (
+          <li key={header}>
+            <a href={`#${header}`}>{header}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
