623f8873a1a4094176cf8c24a35a9c08e39d402f
KIMSOOJIN97 <kipper152@naver.com>
2024-12-03 16:16:31 +0900

    [feat] 카테고리 컴포넌트 적용 - 각 컴포넌트들에서 카테고리 컴포넌트 호출하여 각 영역의 좌상단에  카테고리명 그려지도록 추가

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,22 @@
 import AboutContentComponent from "@/components/about/AboutContentComponent";
 import Image from "next/image";
 import "./about.css";
+import CategoryComponent from "@/components/common/CategoryComponent";
 
 export default function AboutComponent() {
   return (
-    <div className="about-container">
-      <Image
-        src="/images/soojin.jpeg"
-        alt="Soojin Image"
-        width={250}
-        height={50}
-        priority={true}
-      />
-      <AboutContentComponent />
-    </div>
+    <>
+      <CategoryComponent text={"About"} color={"#0072BB"} />
+      <div className="about-container">
+        <Image
+          src="/images/soojin.jpeg"
+          alt="Soojin Image"
+          width={250}
+          height={50}
+          priority={true}
+        />
+        <AboutContentComponent />
+      </div>
+    </>
   );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,5 @@
 // swiper
+import CategoryComponent from "@/components/common/CategoryComponent";
 import "./projects.css";
 // 이미지
 import Image from "next/image";
@@ -26,40 +27,43 @@ export default function ProjectsComponent() {
   ];
 
   return (
-    <div className="project-list">
-      {projectList.map((project, index) => (
-        <div key={index} className="project-card">
-          <div className="image-wrapper">
-            <Image
-              src={project.imgPath}
-              alt={project.imgAltText}
-              layout="fill" // 부모 요소의 크기를 채우도록 설정
-              objectFit="contain" // 이미지가 왜곡되지 않도록 조정
-              priority={true}
-            />
-          </div>
-          <p>{project.title}</p>
-          <p>{project.subtitle}</p>
-          <p className=" text-gray-600 text-base md:text-sm mb-1">
-            {project.startDate} ~ {project.endDate}
-          </p>
-          <div className="flex flex-row items-center text-xs md:text-sm text-gray-700 my-2 ">
-            {project.stacks.map((stack) => (
+    <>
+      <CategoryComponent text={"Projects"} color={"#0072BB"} />
+      <div className="project-list">
+        {projectList.map((project, index) => (
+          <div key={index} className="project-card">
+            <div className="image-wrapper">
               <Image
-                key={stack}
-                className="rounded-sm mx-1"
-                src={`/images/icons/${stack}.svg`}
-                alt={stack}
-                priority
-                quality={100}
-                width={20}
-                height={20}
-                style={{ width: 20, height: 20 }}
+                src={project.imgPath}
+                alt={project.imgAltText}
+                layout="fill" // 부모 요소의 크기를 채우도록 설정
+                objectFit="contain" // 이미지가 왜곡되지 않도록 조정
+                priority={true}
               />
-            ))}
+            </div>
+            <p>{project.title}</p>
+            <p>{project.subtitle}</p>
+            <p className=" text-gray-600 text-base md:text-sm mb-1">
+              {project.startDate} ~ {project.endDate}
+            </p>
+            <div className="flex flex-row items-center text-xs md:text-sm text-gray-700 my-2 ">
+              {project.stacks.map((stack) => (
+                <Image
+                  key={stack}
+                  className="rounded-sm mx-1"
+                  src={`/images/icons/${stack}.svg`}
+                  alt={stack}
+                  priority
+                  quality={100}
+                  width={20}
+                  height={20}
+                  style={{ width: 20, height: 20 }}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 }
