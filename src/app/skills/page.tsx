cf8c46f0f51ad962c972fa1685e39c5af5b5dacf
KIMSOOJIN97 <kipper152@naver.com>
2024-12-06 16:29:32 +0900

    [feat] 클릭된 카테고리 색상 변경되도록 추가
    - skills 페이지 부분에서 선택된 카테고리인 경우 파란색 글자 추가하도록 수정
    - skills 페이지 내부 글꼴 변경

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import CategoryComponent from "@/components/common/CategoryComponent";
 import Image from "next/image";
 import { skillsData } from "@/data/skills/SkillsData";
@@ -16,40 +17,54 @@ export default function SkillsComponent() {
   };
 
   return (
-    <section className="w-full p-10">
+    <section className="w-full">
       <CategoryComponent text={"Skills"} color={"#0072BB"} />
-      <section className="flex pb-2">
-        <Image
-          src={`/images/filter.png`}
-          alt="filter Icon"
-          priority
-          width={30}
-          height={30}
-          style={{ width: 30, height: 30 }}
-        />
-        <ul className="flex space-x-4">
-          {filterConditions.map((condition, index) => (
-            <li key={index} onClick={() => handleClick(condition)}>
-              {condition}
-            </li>
-          ))}
-        </ul>
-      </section>
-      <section>
-        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-          {filteredSkills.map((skill, index) => (
-            <li
-              key={`index`}
-              className="p-4 min-h-[200px] border border-blue-500"
-            >
-              <div>{skill.skill}</div>
-              {skill.descriptions.map((description, index) => (
-                <p key={index}>{description}</p>
-              ))}
-            </li>
-          ))}
-        </ul>
-      </section>
+      <div className="p-10">
+        <section className="flex pb-2">
+          <Image
+            src={`/images/filter.png`}
+            alt="filter Icon"
+            priority
+            width={30}
+            height={30}
+            style={{ width: 30, height: 30 }}
+          />
+          <ul className="flex font-establishRetrosansOTF space-x-4 text-lg">
+            {filterConditions.map((condition, index) => (
+              <li
+                className={`${
+                  condition === selectedType ? "text-customBlue" : "text-white"
+                }`}
+                key={index}
+                onClick={() => handleClick(condition)}
+              >
+                {condition}
+              </li>
+            ))}
+          </ul>
+        </section>
+        <section>
+          <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+            {filteredSkills.map((skill, index) => (
+              <li
+                key={index}
+                className="p-4 min-h-[200px] border border-blue-500"
+              >
+                <div className="text-lg	pb-8 text-center font-SDSamliphopangche_Outline">
+                  {skill.skill}
+                </div>
+                <ul className="list-disc pl-4">
+                  {skill.descriptions.map((description, index) => (
+                    <li className="pb-2 font-GowunBatang_Regular" key={index}>
+                      {description}
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </div>
     </section>
   );
 }
